5fdba6489b81be97bc026460c6024533ff060c70
ytsaplin <yuri.a.tsaplin@gmail.com>
2022-06-11 21:15:49 +0200

    indexed CORDEX

---
+++ scripts/walk_dirs.py
@@ -0,0 +1,13 @@
+import os
+
+path = "datasets-aggregated-regionally/data/CORDEX"
+
+
+with open(os.path.join(os.path.abspath(path), "index.txt"), 'w') as f:
+
+    for dirpath, dirnames, filenames in os.walk(path):
+        for name in filenames:
+            #print(os.path.join(dirpath, name))
+            f.write(os.path.join(dirpath, name)+"\n")
+
+
